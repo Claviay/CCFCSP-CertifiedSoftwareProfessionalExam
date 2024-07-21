a225048c5b96efb9f48ac39bd7139219c642e36c
28580 <28580@DESKTOP-UOITD0I>
2024-07-21 14:34:07 +0800

    矩阵运算 1.0

---
+++ A2023/D05_2/Main.java
@@ -0,0 +1,9 @@
+package D05_2;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = new String();
+        str += "s" + "ddd";
+        System.out.println(str);
+    }
+}
