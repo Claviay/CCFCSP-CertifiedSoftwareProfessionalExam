e06d52e323df7d607f4df29ec0a8a2c6ad1a1b41
Claviay <137407542+Claviay@users.noreply.github.com>
2024-09-20 14:51:03 +0800

    Update 相似度计算33_2.java

--- A2023/Temp/相似度计算33_2.java
+++ A2023/Temp/相似度计算33_2.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.util.*;
-
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] input = br.readLine().split(" ");
@@ -26,21 +25,28 @@ public class Main {
         int book1Size = book1Set.size();
         int book2Size = book2Set.size();
 
-        int bothInBooks = 0;
-        for (String book : book1Set) {
-            if (book2Set.contains(book)) {
-                bothInBooks++;
-            }
-        }
+//        int bothInBooks = 0;
+//        for (String book : book1Set) {
+//            if (book2Set.contains(book)) {
+//                bothInBooks++;
+//            }
+//        }
+//
+//        int totalDifferentWordsNum = book1Size + book2Size - bothInBooks;
+//
+//
+//        System.out.println(bothInBooks);
+//        System.out.print(totalDifferentWordsNum);
 
-        int totalDifferentWordsNum = book1Size + book2Size - bothInBooks;
+        int bothInBooks;
+        int totalDifferentWordsNum;
 
+        book1Set.retainAll(book2Set);
+        bothInBooks = book1Set.size();
+        totalDifferentWordsNum = book1Size + book2Size - bothInBooks;
 
         System.out.println(bothInBooks);
         System.out.print(totalDifferentWordsNum);
-
-
-
     }
 
 
